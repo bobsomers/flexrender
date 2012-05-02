8be1bb90357264d212c7a87126b8fedb30a6b5bf
Bob Somers <bob@bobsomers.com>
2012-05-02 06:35:33 -0700

    Fixed some pretty bad bugs in the library.

--- src/shared/utils/library.cpp
+++ src/shared/utils/library.cpp
@@ -30,6 +30,22 @@ Library::Library() :
 Library::~Library() {
     if (_config != nullptr) delete _config;
     if (_camera != nullptr) delete _camera;
+
+    for (size_t i = 0; i < _shaders.size(); i++) {
+        if (_shaders[i] != nullptr) delete _shaders[i];
+    }
+    for (size_t i = 0; i < _textures.size(); i++) {
+        if (_textures[i] != nullptr) delete _textures[i];
+    }
+    for (size_t i = 0; i < _materials.size(); i++) {
+        if (_materials[i] != nullptr) delete _materials[i];
+    }
+    for (size_t i = 0; i < _meshes.size(); i++) {
+        if (_meshes[i] != nullptr) delete _meshes[i];
+    }
+    for (size_t i = 0; i < _nodes.size(); i++) {
+        if (_nodes[i] != nullptr) delete _nodes[i];
+    }
 }
 
 void Library::StoreConfig(Config* config) {
@@ -44,63 +60,48 @@ void Library::StoreCamera(Camera* camera) {
 
 void Library::StoreShader(uint64_t id, Shader* shader) {
     if (id < _shaders.size()) {
-        if (_shaders[id] != nullptr) {
-            delete _shaders[id];
-            _shaders[id] = nullptr;
-        }
+        if (_shaders[id] != nullptr) delete _shaders[id];
     } else {
-        _shaders.resize(id, nullptr);
+        _shaders.resize(id + 1, nullptr);
     }
-    _shaders.insert(_shaders.begin() + id, shader);
+    _shaders[id] = shader;
 }
 
 void Library::StoreTexture(uint64_t id, Texture* texture) {
     if (id < _textures.size()) {
-        if (_textures[id] != nullptr) {
-            delete _textures[id];
-            _textures[id] = nullptr;
-        }
+        if (_textures[id] != nullptr) delete _textures[id];
     } else {
-        _textures.resize(id, nullptr);
+        _textures.resize(id + 1, nullptr);
     }
-    _textures.insert(_textures.begin() + id, texture);
+    _textures[id] = texture;
 }
 
 void Library::StoreMaterial(uint64_t id, Material* material, const string& name) {
     if (id < _materials.size()) {
-        if (_materials[id] != nullptr) {
-            delete _materials[id];
-            _materials[id] = nullptr;
-        }
+        if (_materials[id] != nullptr) delete _materials[id];
     } else {
-        _materials.resize(id, nullptr);
+        _materials.resize(id + 1, nullptr);
     }
-    _materials.insert(_materials.begin() + id, material);
+    _materials[id] = material;
     _material_name_index[name] = id;
 }
 
 void Library::StoreMesh(uint64_t id, Mesh* mesh) {
     if (id < _meshes.size()) {
-        if (_meshes[id] != nullptr) {
-            delete _meshes[id];
-            _meshes[id] = nullptr;
-        }
+        if (_meshes[id] != nullptr) delete _meshes[id];
     } else {
-        _meshes.resize(id, nullptr);
+        _meshes.resize(id + 1, nullptr);
     }
-    _meshes.insert(_meshes.begin() + id, mesh);
+    _meshes[id] = mesh;
 }
 
 void Library::StoreNetNode(uint64_t id, NetNode* node) {
     if (id < _nodes.size()) {
-        if (_nodes[id] != nullptr) {
-            delete _nodes[id];
-            _nodes[id] = nullptr;
-        }
+        if (_nodes[id] != nullptr) delete _nodes[id];
     } else {
-        _nodes.resize(id, nullptr);
+        _nodes.resize(id + 1, nullptr);
     }
-    _nodes.insert(_nodes.begin() + id, node);
+    _nodes[id] = node;
 }
 
 void Library::ForEachNetNode(function<void (uint64_t, NetNode* node)> func) {
