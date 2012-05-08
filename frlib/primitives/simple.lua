e2b790ac217656b8ed52047426151a7d3e0f56de
Bob Somers <bob@bobsomers.com>
2012-05-08 06:13:53 -0700

    Fixed bug in cube.

--- frlib/primitives/simple.lua
+++ frlib/primitives/simple.lua
@@ -9,179 +9,178 @@ local geometric = require "base.geometric"
 -- Module aliases.
 local vec2 = types.vec2
 local vec3 = types.vec3
-local normalize = geometric.normalize
 
 -- Creates a function that returns geometry for a cube with sides of length n
 -- centered at the origin.
 local function cube(n)
     return function()
-        local half_n = n / 2
+        local h = n / 2
 
         -- Front face.
         triangle {{
-            v = vec3(half_n, -half_n, half_n),
-            n = normalize(vec3(1, -1, 1)),
+            v = vec3(-h, -h, h),
+            n = vec3(0, 0, 1),
             t = vec2(0, 0)
         }, {
-            v = vec3(half_n, half_n, half_n),
-            n = normalize(vec3(1, 1, 1)),
+            v = vec3(-h, h, h),
+            n = vec3(0, 0, 1),
             t = vec2(0, 1)
         }, {
-            v = vec3(half_n, -half_n, half_n),
-            n = normalize(vec3(1, -1, 1)),
+            v = vec3(h, -h, h),
+            n = vec3(0, 0, 1),
             t = vec2(1, 0)
         }}
         triangle {{
-            v = vec3(half_n, half_n, half_n),
-            n = normalize(vec3(1, 1, 1)),
+            v = vec3(h, h, h),
+            n = vec3(0, 0, 1),
             t = vec2(1, 1)
         }, {
-            v = vec3(half_n, -half_n, half_n),
-            n = normalize(vec3(1, -1, 1)),
+            v = vec3(h, -h, h),
+            n = vec3(0, 0, 1),
             t = vec2(1, 0)
         }, {
-            v = vec3(half_n, half_n, half_n),
-            n = normalize(vec3(1, 1, 1)),
+            v = vec3(-h, h, h),
+            n = vec3(0, 0, 1),
             t = vec2(0, 1)
         }}
 
         -- Back face.
         triangle {{
-            v = vec3(half_n, -half_n, -half_n),
-            n = normalize(vec3(1, -1, -1)),
+            v = vec3(h, -h, -h),
+            n = vec3(0, 0, -1),
             t = vec2(0, 0)
         }, {
-            v = vec3(half_n, half_n, -half_n),
-            n = normalize(vec3(1, 1, -1)),
+            v = vec3(h, h, -h),
+            n = vec3(0, 0, -1),
             t = vec2(0, 1)
         }, {
-            v = vec3(half_n, -half_n, -half_n),
-            n = normalize(vec3(1, -1, -1)),
+            v = vec3(-h, -h, -h),
+            n = vec3(0, 0, -1),
             t = vec2(1, 0)
         }}
         triangle {{
-            v = vec3(half_n, half_n, -half_n),
-            n = normalize(vec3(1, 1, -1)),
+            v = vec3(-h, h, -h),
+            n = vec3(0, 0, -1),
             t = vec2(1, 1)
         }, {
-            v = vec3(half_n, -half_n, -half_n),
-            n = normalize(vec3(1, -1, -1)),
+            v = vec3(-h, -h, -h),
+            n = vec3(0, 0, -1),
             t = vec2(1, 0)
         }, {
-            v = vec3(half_n, half_n, -half_n),
-            n = normalize(vec3(1, 1, -1)),
+            v = vec3(h, h, -h),
+            n = vec3(0, 0, -1),
             t = vec2(0, 1)
         }}
 
         -- Left face.
         triangle {{
-            v = vec3(half_n, -half_n, -half_n),
-            n = normalize(vec3(1, -1, -1)),
+            v = vec3(-h, -h, -h),
+            n = vec3(-1, 0, 0),
             t = vec2(0, 0)
         }, {
-            v = vec3(half_n, half_n, -half_n),
-            n = normalize(vec3(1, 1, -1)),
+            v = vec3(-h, h, -h),
+            n = vec3(-1, 0, 0),
             t = vec2(0, 1)
         }, {
-            v = vec3(half_n, -half_n, half_n),
-            n = normalize(vec3(1, -1, 1)),
+            v = vec3(-h, -h, h),
+            n = vec3(-1, 0, 0),
             t = vec2(1, 0)
         }}
         triangle {{
-            v = vec3(half_n, half_n, half_n),
-            n = normalize(vec3(1, 1, 1)),
+            v = vec3(-h, h, h),
+            n = vec3(-1, 0, 0),
             t = vec2(1, 1)
         }, {
-            v = vec3(half_n, -half_n, half_n),
-            n = normalize(vec3(1, -1, 1)),
+            v = vec3(-h, -h, h),
+            n = vec3(-1, 0, 0),
             t = vec2(1, 0)
         }, {
-            v = vec3(half_n, half_n, -half_n),
-            n = normalize(vec3(1, 1, -1)),
+            v = vec3(-h, h, -h),
+            n = vec3(-1, 0, 0),
             t = vec2(0, 1)
         }}
 
         -- Right face.
         triangle {{
-            v = vec3(half_n, -half_n, half_n),
-            n = normalize(vec3(1, -1, 1)),
+            v = vec3(h, -h, h),
+            n = vec3(1, 0, 0),
             t = vec2(0, 0)
         }, {
-            v = vec3(half_n, half_n, half_n),
-            n = normalize(vec3(1, 1, 1)),
+            v = vec3(h, h, h),
+            n = vec3(1, 0, 0),
             t = vec2(0, 1)
         }, {
-            v = vec3(half_n, -half_n, -half_n),
-            n = normalize(vec3(1, -1, -1)),
+            v = vec3(h, -h, -h),
+            n = vec3(1, 0, 0),
             t = vec2(1, 0)
         }}
         triangle {{
-            v = vec3(half_n, half_n, -half_n),
-            n = normalize(vec3(1, 1, -1)),
+            v = vec3(h, h, -h),
+            n = vec3(1, 0, 0),
             t = vec2(1, 1)
         }, {
-            v = vec3(half_n, -half_n, -half_n),
-            n = normalize(vec3(1, -1, -1)),
+            v = vec3(h, -h, -h),
+            n = vec3(1, 0, 0),
             t = vec2(1, 0)
         }, {
-            v = vec3(half_n, half_n, half_n),
-            n = normalize(vec3(1, 1, 1)),
+            v = vec3(h, h, h),
+            n = vec3(1, 0, 0),
             t = vec2(0, 1)
         }}
 
         -- Top face.
         triangle {{
-            v = vec3(half_n, half_n, half_n),
-            n = normalize(vec3(1, 1, 1)),
+            v = vec3(-h, h, h),
+            n = vec3(0, 1, 0),
             t = vec2(0, 0)
         }, {
-            v = vec3(half_n, half_n, -half_n),
-            n = normalize(vec3(1, 1, -1)),
+            v = vec3(-h, h, -h),
+            n = vec3(0, 1, 0),
             t = vec2(0, 1)
         }, {
-            v = vec3(half_n, half_n, half_n),
-            n = normalize(vec3(1, 1, 1)),
+            v = vec3(h, h, h),
+            n = vec3(0, 1, 0),
             t = vec2(1, 0)
         }}
         triangle {{
-            v = vec3(half_n, half_n, -half_n),
-            n = normalize(vec3(1, 1, -1)),
+            v = vec3(h, h, -h),
+            n = vec3(0, 1, 0),
             t = vec2(1, 1)
         }, {
-            v = vec3(half_n, half_n, half_n),
-            n = normalize(vec3(1, 1, 1)),
+            v = vec3(h, h, h),
+            n = vec3(0, 1, 0),
             t = vec2(1, 0)
         }, {
-            v = vec3(half_n, half_n, -half_n),
-            n = normalize(vec3(1, 1, -1)),
+            v = vec3(-h, h, -h),
+            n = vec3(0, 1, 0),
             t = vec2(0, 1)
         }}
 
         -- Bottom face.
         triangle {{
-            v = vec3(half_n, -half_n, -half_n),
-            n = normalize(vec3(1, -1, -1)),
+            v = vec3(-h, -h, -h),
+            n = vec3(0, -1, 0),
             t = vec2(0, 0)
         }, {
-            v = vec3(half_n, -half_n, half_n),
-            n = normalize(vec3(1, -1, 1)),
+            v = vec3(-h, -h, h),
+            n = vec3(0, -1, 0),
             t = vec2(0, 1)
         }, {
-            v = vec3(half_n, -half_n, -half_n),
-            n = normalize(vec3(1, -1, -1)),
+            v = vec3(h, -h, -h),
+            n = vec3(0, -1, 0),
             t = vec2(1, 0)
         }}
         triangle {{
-            v = vec3(half_n, -half_n, half_n),
-            n = normalize(vec3(1, -1, 1)),
+            v = vec3(h, -h, h),
+            n = vec3(0, -1, 0),
             t = vec2(1, 1)
         }, {
-            v = vec3(half_n, -half_n, -half_n),
-            n = normalize(vec3(1, -1, -1)),
+            v = vec3(h, -h, -h),
+            n = vec3(0, -1, 0),
             t = vec2(1, 0)
         }, {
-            v = vec3(half_n, -half_n, half_n),
-            n = normalize(vec3(1, -1, 1)),
+            v = vec3(-h, -h, h),
+            n = vec3(0, -1, 0),
             t = vec2(0, 1)
         }}
     end
