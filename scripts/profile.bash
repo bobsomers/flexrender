e366f3def87ef5ab315ddf23201a4b1abd9008a6
Bob Somers <bob@bobsomers.com>
2012-05-16 16:11:33 -0700

    Kernel upgrade.

--- scripts/profile.bash
+++ scripts/profile.bash
@@ -2,7 +2,7 @@
 
 sudo opcontrol --deinit
 echo 0 | sudo tee /proc/sys/kernel/nmi_watchdog
-sudo opcontrol --vmlinux=/usr/src/linux-3.3.5-1-ARCH/vmlinux
+sudo opcontrol --vmlinux=/usr/src/linux-3.3.6-1-ARCH/vmlinux
 sudo opcontrol --separate=kernel
 sudo opcontrol --init
 sudo opcontrol --reset
