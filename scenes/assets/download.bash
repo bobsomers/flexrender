64dfe44ab85206327a9d912b233a0642c4216bdc
Bob Somers <bob@bobsomers.com>
2012-05-20 01:21:06 -0700

    Core mesh assets and download script.

---
+++ scenes/assets/download.bash
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+# Where are we in the filesystem?
+BASEPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function fetch {
+    if [ ! -f $BASEPATH/$1 ]; then
+        echo ""
+        echo "==> Downloading $1.xz"
+        wget -O $BASEPATH/$1.xz http://flexrender.org/files/assets/$1.xz
+
+        echo ""
+        echo "==> Unpacking $1.xz"
+        xz --decompress $BASEPATH/$1.xz
+    fi
+}
+
+echo "======================================================================"
+echo "    DOWNLOADING ASSETS @ "`date`
+echo "======================================================================"
+
+fetch "buddha-hi.obj"
+fetch "buddha-lo.obj"
+fetch "bunny-hi.obj"
+fetch "bunny-lo.obj"
+fetch "dragon-hi.obj"
+fetch "dragon-lo.obj"
+
+echo ""
+echo "======================================================================"
+echo "    DOWNLOAD COMPLETE @ "`date`
+echo "======================================================================"
