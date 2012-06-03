9fa6261a391b4cddf726e4eb70dc957c2eaeacb9
Bob Somers <bob@bobsomers.com>
2012-06-02 17:06:19 -0700

    Toy shelf.

--- scenes/toystore.lua
+++ scenes/toystore.lua
@@ -12,9 +12,24 @@ local scale = fr.scale
 local rotate = fr.rotate
 local translate = fr.translate
 
+local mat_unique = 0
+function next_mat()
+    mat_unique = mat_unique + 1
+    return "mat-" .. mat_unique
+end
+
+math.randomseed(42)
+
+--[[
 camera {
     eye = vec3(50, 30, 50),
     look = vec3(0, 0, 0)
+}
+]]
+
+camera {
+    eye = vec3(7, 7, 7),
+    look = vec3(0, 2, 0)
 }
 
 material {
@@ -25,7 +40,7 @@ material {
 
 mesh {
     material = "light",
-    transform = translate(vec3(0, 50, 0)) * rotate(radians(90), vec3(1, 0, 0)),
+    transform = translate(vec3(0, 50, 50)) * rotate(radians(135), vec3(1, 0, 0)),
     data = fre.plane(1)
 }
 
@@ -76,6 +91,86 @@ function draw_stair(n, y)
     }
 end
 
+function draw_toy(xform, i, j, shelf)
+    local color = vec3(math.random() * 0.5 + 0.25,
+                       math.random() * 0.5 + 0.25,
+                       math.random() * 0.5 + 0.25)
+    local shader = fre.phong(0.6, color, 0.2, color, 0.2, 8)
+    local mat = next_mat()
+
+    local size = 1
+    local data = nil
+    local facing = radians(math.random(0, 359))
+    local model = math.random(1, 3)
+    if model == 1 then
+        data = fre.obj("scenes/assets/bunny-lo.obj", true)
+        size = 0.3
+    elseif model == 2 then
+        data = fre.obj("scenes/assets/buddha-lo.obj", true)
+        size = 0.6
+    else
+        data = fre.obj("scenes/assets/dragon-lo.obj", true)
+        size = 0.3
+    end
+
+    material {
+        name = mat,
+        emissive = false,
+        shader = shader
+    }
+
+    mesh {
+        material = mat,
+        transform = translate(vec3(i - 2, shelf * 2 + 0.5 + size, j - 0.5)) * rotate(facing, vec3(0, 1, 0)) * scale(size),
+        data = data
+    }
+end
+
+function draw_shelf()
+    -- Bottom shelf.
+    mesh {
+        material = "red",
+        transform = translate(vec3(0, 0.5, 0)) * rotate(radians(-90), vec3(1, 0, 0)) * scale(vec3(5, 2, 2)),
+        data = fre.plane(1)
+    }
+
+    -- Middle shelf.
+    mesh {
+        material = "red",
+        transform = translate(vec3(0, 2.5, 0)) * rotate(radians(-90), vec3(1, 0, 0)) * scale(vec3(5, 2, 2)),
+        data = fre.plane(1)
+    }
+
+    -- Top shelf.
+    mesh {
+        material = "red",
+        transform = translate(vec3(0, 4.5, 0)) * rotate(radians(-90), vec3(1, 0, 0)) * scale(vec3(5, 2, 2)),
+        data = fre.plane(1)
+    }
+
+    -- Posts.
+    for i = -1, 1, 2 do
+        for j = -1, 1, 2 do
+            mesh {
+                material = "red",
+                transform = translate(vec3(i * 2.5, 2.5, j)) * scale(vec3(0.25, 5, 0.25)),
+                data = fre.cube(1)
+            }
+        end
+    end
+end
+
+draw_shelf()
+for i = 0, 4 do
+    for j = 0, 1 do
+        for shelf = 0, 2 do
+            draw_toy(mat4(), i, j, shelf)
+        end
+    end
+end
+
+--[[
+
 -- ==== STAIRS ====
 
 for i = 1, 10 do
@@ -102,3 +197,5 @@ for i = 0, 30 do
         draw_floor_tile(i, j, 0)
     end
 end
+
+]]
